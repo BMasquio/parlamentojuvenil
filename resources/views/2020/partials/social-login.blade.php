00e390d0ca0d00e3575be8c209a0bb2ca96f6225
BMasquio <brunomasquio@gmail.com>
2021-02-02 16:25:02 -0300

    Fix middleware. Working on routes

--- resources/views/2020/partials/social-login.blade.php
+++ resources/views/2020/partials/social-login.blade.php
@@ -8,16 +8,4 @@
         <span class="fab fa-fw fa-twitter"></span>
         Entrar com Twitter
     </a>
-    <a href="{{ route('auth.social.redirect', ['instagram']) }}"  class="btn btn-block btn-social btn-instagram">
-        <span class="fab fa-fw fa-instagram"></span>
-        Entrar com Instagram
-    </a>
-    <a href="{{ route('auth.social.redirect', ['linkedin']) }}"  class="btn btn-block btn-social btn-linkedin">
-        <span class="fab fa-fw fa-linkedin"></span>
-        Entrar com Linkedin
-    </a>
-{{--    <a href="{{ route('auth.social.redirect', ['youtube']) }}" class="btn btn-block btn-social btn-youtube">--}}
-{{--        <span class="fab fa-fw fa-youtube-square"></span>--}}
-{{--        Entrar com Youtube--}}
-{{--    </a>--}}
 </div>
